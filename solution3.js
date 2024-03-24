cab4ad4688c7ca21638753064968965aefc2f673
Abdimahad hassan salat <mahadgaceyte1@gmail.com>
2024-03-25 00:39:37 +0300

    confused

--- solution3.js
+++ solution3.js
@@ -1,6 +1,6 @@
 function taxRate(taxableIncome){
 
-    if(taxableincome >=0 && taxableIncome<= 24000){
+    if(taxableIncome>=0 && taxableIncome<=24000){
         return (0.1*taxableIncome)
     
     }
@@ -15,8 +15,10 @@ function taxRate(taxableIncome){
     else if(taxableIncome>=500001 && taxableIncome<=800000){
         return (0.325*taxableIncome)
     }
-    else{}
-    return (0.35*taxableIncome)
+    else{
+        return (0.35*taxableIncome)
+    }
+    
 }   
 
 function nhifRate(grossSalary){
@@ -27,16 +29,46 @@ function nhifRate(grossSalary){
         return (300)
         
     }
-    else if(grossSalary>=)
+    else if(grossSalary>=8000 && grossSalary <=11999){
+        return (400)
+    }
+    else if(grossSalary>=12000 && grossSalary<=14999){
+        return(500)
+    }
+    else if(grossSalary>=15000 && grossSalary <=19999){
+        return (600)
 
-
-
-
-
-
+    }
+    else if(grossSalary>=20000 && grossSalary <=24999){
+        return(750)
+    }
+    else if(grossSalary>=25000 && grossSalary<=29999){
+        return(850)
+    }
+    else if(grossSalary>=30000 && grossSalary<=34999){
+        return (900)
+    
+    }
+    else if (grossSalary>=35000 && grossSalary<=39999){
+        return (950)
+    }
+    
 
 }
 
+function calculateNetsalary (basicsalary,benefits){
+    const grossSalary=basicsalary+benefits
+    const nssfRate=basicsalary*0.06
+    const nhif=nhifRate(grossSalary)
+    const taxableIncome=basicsalary-nhif-nssfRate
+    const PAYE=taxRate(taxableIncome)-2400
+    console.log(nhif)
+    const totalDeduction=nhif+nssfRate+PAYE
+    const netSalary=basicsalary-totalDeduction
+    return (netSalary)
+
+}
+    console.log(calculateNetsalary(100000,5000))
 
 
 
@@ -49,17 +81,3 @@ function nhifRate(grossSalary){
 
 
 
-function calculateNetsalary (basicsalary,benefits)(
-
-
-
-
-
-
-
-
-
-
-
-
-)
